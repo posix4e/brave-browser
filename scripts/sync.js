0bd393e47aacec05e701d07cab75084addf670d8
bridiver <github@brianjohnson.cc>
2018-10-24 13:48:21 -0700

    wip

--- scripts/sync.js
+++ scripts/sync.js
@@ -4,8 +4,6 @@ const fs = require('fs-extra')
 const config = require('../lib/config')
 const util = require('../lib/util')
 
-const projectNames = config.projectNames.filter((project) => config.projects[project].ref)
-
 program
   .version(process.env.npm_package_version)
   .option('--gclient_file <file>', 'gclient config file location')
@@ -15,10 +13,8 @@ program
   .option('--submodule_sync', 'run submodule sync')
   .option('--init', 'initialize all dependencies')
   .option('--all', 'update all projects')
-projectNames.forEach((name) => {
-  let project = config.projects[name]
-  program.option('--' + project.arg_name + '_ref <ref>', name + ' ref to checkout')
-})
+  .option('--android', 'initialize/update android browser')
+  .option('--break_repo_locks', 'gclient break repo locks')
 
 program.parse(process.argv)
 config.update(program)
@@ -31,24 +27,20 @@ if (program.init) {
   util.buildGClientConfig()
 }
 
-if (program.init) {
-  util.gclientSync(true)
+let projectRefs = []
+if (program.init || program.all) {
+  config.projectNames.forEach((name) => {
+    const project = config.projects[name]
+    projectRefs =projectRefs.concat(['-r', project.gclientName + '@' + project.ref])
+  })
+
+  let args = projectRefs
+  if (program.break_repo_locks)
+    args.push('--break_repo_locks')
+
+  util.gclientSync(true, args)
 }
 
-let updatedVersion = false
-
-projectNames.forEach((name) => {
-  let project = config.projects[name]
-  if (program.init || program.all || program[project.arg_name + '_ref']) {
-    updatedVersion = true
-    util.setDepVersion(project.dir, project.ref)
-  }
-})
-
-if (updatedVersion || program.init || program.run_sync) {
-  util.gclientSync()
-}
-
-if (updatedVersion || program.init || program.run_hooks) {
+if (program.run_hooks) {
   util.gclientRunhooks()
 }
